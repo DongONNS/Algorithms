e1f99fbf4d794c7d987ece9aa77123f4a9851dd7
dongcheng2019 <dongcheng2018@163.com>
2020-02-22 20:28:53 +0800

    heapsort

--- src/main/java/edu/csu/Heap.java
+++ src/main/java/edu/csu/Heap.java
@@ -15,7 +15,7 @@ public class Heap {
         }
     }
 
-    private void sink(Comparable[] pq,int k,int n){
+    private static void sink(Comparable[] pq,int k,int n){
         while(2 * k < n){
             int j = 2 * k;
             if (j < n && less(pq, j,j+1)) j++;
@@ -25,11 +25,11 @@ public class Heap {
         }
     }
 
-    private boolean less(Comparable[] pq,int i,int j){
+    private static boolean less(Comparable[] pq,int i,int j){
         return pq[i-1].compareTo(pq[j-1]) < 0;
     }
 
-    private void exch(Object[] pq,int i,int j){
+    private static void exch(Object[] pq,int i,int j){
         Object swap = pq[i-1];
         pq[i-1] = pq[j-1];
         pq[j-1] = swap;
